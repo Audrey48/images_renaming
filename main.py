80780cadf65d8d94896a44b093b1f5049402e33e
Audrey <aud.boulard@gmail.com>
2018-10-16 18:26:29 +0200

    Copy pictures in result directory only if there is no conflict. Move copied pictures in "copied" directory

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from fnmatch import fnmatch 
 import shutil
+import filecmp
 
 from PIL import Image
 from PIL.ExifTags import TAGS
@@ -30,8 +31,12 @@ class JpegImage:
 if __name__ == '__main__':
     input_path = ".\\pictures"
     output_path = ".\\result"
+    copied_path = ".\\copied"
     pattern = "*.jpg"
     my_img_dictionary = dict()
+    duplicated_files = 0
+    input_conflicts = 0
+    output_conflicts = 0
 
     #parse all images
     for element in os.listdir(input_path):
@@ -46,22 +51,56 @@ if __name__ == '__main__':
             else:
                 my_img_dictionary[time] = [img_name]
 
-    #rename images
+    #rename and move images
     if not os.path.exists(output_path):
         os.makedirs(output_path)
+    
+    if not os.path.exists(copied_path):
+        os.makedirs(copied_path)
 
     for x, y in my_img_dictionary.items():
+        conflict = False
         print(x, y)
 
-        if len(y) != 1 :
-            print("Too many images for key " + x)
+        time_name = x.replace(":", "-")
+        time_name = time_name.replace(" ", "_")
+        time_name = time_name + ".JPG"
+        print(time_name)
 
+        if not os.path.exists(".\\result\\"+time_name):
+            #print("save renamed image in output directory")
+            shutil.copy2(y[0], ".\\result\\"+time_name)
         else:
-            name = x.replace(":", "-")
-            name = name.replace(" ", "_")
-            name = name + ".JPG"
-            #print(name)
-            shutil.copy2(y[0], ".\\result\\"+name)
+            if not filecmp.cmp(y[0], ".\\result\\"+time_name, shallow=False) :
+                print("File already exists in output directory but differs")
+                output_conflicts += 1
+                conflict = True
 
+        if not conflict:
+            #print("move original image")
+            if not os.path.exists(y[0].replace(input_path, copied_path)):
+                shutil.move(y[0], y[0].replace(input_path, copied_path))
+            else:
+                if filecmp.cmp(y[0], y[0].replace(input_path, copied_path)):
+                    #print("file already exists in copied directory but is the same")
+                    shutil.move(y[0], y[0].replace(input_path, copied_path))
+           
+            if len(y) != 1 :
+                print("Too many images for key " + x)
+                for i in range (1, len(y)):
+                    print(y[0] + " and " + y[i])
+                    compare = filecmp.cmp(y[0].replace(input_path, copied_path), y[i], shallow=False)
+                    if compare == True :
+                        print("Files are the same")
+                        duplicated_files += 1
+                        shutil.move(y[i], y[i].replace(input_path, copied_path))
+
+                    else:
+                        print("Files differ")
+                        input_conflicts += 1
+
+    print("duplicated files : " + str(duplicated_files))
+    print("input_conflicts : " + str(input_conflicts))
+    print("output_conflicts : " + str(output_conflicts))
 
     
