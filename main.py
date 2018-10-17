57b4b64b1f63ea766653c042a113fab37a9f3e6a
Audrey <aud.boulard@gmail.com>
2018-10-17 15:33:08 +0200

    Manage errors if the image has a bad format (cannot be opened, no exif, no DateTime data...). All images with these kind of problems are moved to "issues" folder

--- main.py
+++ main.py
@@ -11,45 +11,74 @@ class JpegImage:
     def __init__(self, filename=None, exif=None):
         self.filename = filename
         self.exif = exif
+        self.time = None
  
     def get_exif(self):
         ret = {}
-        i = Image.open(self.filename)
+        try:
+            i = Image.open(self.filename)
+        except:
+            print('Skipping unreadable image' + self.filename)
+            return
         self.exif = i._getexif()
         #print('info: {}'.format(self.exif))
         return self.exif
 
     def get_field(self,field) :
-      if self.exif == None :
-          self.get_exif()
-          
-      for (tag_id,field_value) in self.exif.items():
-         if TAGS.get(tag_id) == field:
-            return field_value
+        if self.exif == None :
+            self.get_exif()
+      
+        if self.exif == None :
+            print("Cannot extract exif from " + self.filename)
+            return False
+
+        for (tag_id,field_value) in self.exif.items():
+            #print("Tag : " + str(TAGS.get(tag_id)) + " ; value : " + str(field_value))
+            if TAGS.get(tag_id) == field :
+                self.time = field_value
+        
+        if self.time == None :
+            print("No time attribute for " + self.filename)
+            return False
+        
+        return True
 
  
 if __name__ == '__main__':
     input_path = ".\\pictures"
     output_path = ".\\result"
     copied_path = ".\\copied"
-    pattern = "*.jpg"
+    issues_path = ".\\issues"
+    pattern = "*.jpeg"
     my_img_dictionary = dict()
+    input_issues = 0
     duplicated_files = 0
     input_conflicts = 0
     output_conflicts = 0
 
+    
+    if not os.path.exists(issues_path):
+        os.makedirs(issues_path)
+
     #parse all images
     for element in os.listdir(input_path):
         if fnmatch(element, pattern):
+            issue_encountered = False
             img_name = input_path + '\\' + element
-
+            print(img_name)
             myImage = JpegImage(filename=img_name)
-            time = myImage.get_field('DateTime')
+            ret = myImage.get_field('DateTime')
 
-            if time in my_img_dictionary:
-                my_img_dictionary[time].append(img_name)
-            else:
-                my_img_dictionary[time] = [img_name]
+            if not ret:
+                print("Error while extracting data from " + img_name)
+                shutil.move(img_name, img_name.replace(input_path, issues_path))
+                issue_encountered = True
+
+            if not issue_encountered :
+                if myImage.time in my_img_dictionary:
+                    my_img_dictionary[myImage.time].append(img_name)
+                else:
+                    my_img_dictionary[myImage.time] = [img_name]
 
     #rename and move images
     if not os.path.exists(output_path):
